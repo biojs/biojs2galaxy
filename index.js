32bb5bf4d2a67aa724ef158073e331d2be43b443
Sebastian Wilzbach <seb@wilzba.ch>
2015-05-08 17:38:04 +0200

    added local mode to build packages

--- index.js
+++ index.js
@@ -23,9 +23,10 @@ program
   .version(require(__dirname + '/package.json').version)
   .usage('[options] [files]')
   .option('-v, --verbose', 'Increase verbosity', false)
+  .option('-p, --path [path]', 'Path to local package')
   .parse(process.argv);
 
-if(program.verbose){
+if (program.verbose) {
   minilog.enable();
 }
 
@@ -39,52 +40,95 @@ if (program.args.length > 0) {
 
 var tmpPath = ""; // will be set automatically
 
-var instance = new NpmList(['galaxy-biojs', 'galaxy-vis'], function(pkgs) {
-  console.log("All packages have been installed. Congrats!");
-});
 
-var installer = new NpmInstall();
+var tmp = require('tmp');
 
-// the interaction between the workmen <-> npmInstaller and makoBrowserify is event-based
-// here we define the routes the wire our tools together
+// local debug mode - use the 
+var localMode = "path" in program;
+if (localMode) {
 
-// we have seen a  package -> begin to install
-instance.on("single-pkg-start", installer.install.bind(installer));
+  if (program.path.length == 0) {
+    program.path = process.cwd();
+  }
+  if (program.path[0] != "/") {
+    program.path = process.cwd() + "/" + program.path;
+  }
 
-// all keywords haven been downloaded
-instance.on("all-pkg-start", function(pkgs) {
-  console.log("#pkgs: ", pkgs.length);
-});
+  function start() {
+    var pkg = require(program.path + "/package.json");
+    console.log(pkg.name + ": starting -> mako");
+    var inst = new MakoBrowserify({
+      path: outFolder,
+      tmpPath: program.path,
+      local: true
+    }, pkg);
+    inst.build().then(function() {
+      console.log("saved to ", outFolder);
+    }, function(err) {
+      console.log("Error:", err);
+    });
+  }
 
-// package has been installed -> begin the mako toolchain
-instance.on("installed-pkg", function(pkg) {
-  console.log(pkg.name + ": finished -> mako");
-  var inst = new MakoBrowserify({
-    path: outFolder,
-    tmpPath: tmpPath
-  }, pkg);
-  inst.build().then(function() {
-    instance.trigger("done-pkg", pkg);
-  }, function(err) {
-    console.log("Error:", err);
+  fsp.remove(outFolder)
+    .then(function() {
+      return fsp.mkdirp(outFolder);
+    }).then(function() {
+      start();
+    }, function(err) {
+      console.log("Error:", err);
+    });
+
+} else {
+
+  var tagList = ['galaxy-biojs', 'galaxy-vis'];
+  var instance = new NpmList(tagList, function(pkgs) {
+    console.log("All packages have been installed. Congrats!");
   });
-});
 
-// a single package has been finalised
-instance.on("done-pkg", function(pkg) {
-  console.log(pkg.name + ": success");
-});
+  var installer = new NpmInstall();
 
-// lets start the party
+  // the interaction between the workmen <-> npmInstaller and makoBrowserify is event-based
+  // here we define the routes the wire our tools together
 
-fsp.remove(outFolder)
-  .then(function() {
-    return fsp.mkdirp(outFolder);
-  }).then(function() {
-    return installer.init();
-  }).then(function() {
-    tmpPath = installer.path;
-    instance.start();
-  }, function(err) {
-    console.log("Error:", err);
+  // we have seen a  package -> begin to install
+  instance.on("single-pkg-start", installer.install.bind(installer));
+
+  // all keywords haven been downloaded
+  instance.on("all-pkg-start", function(pkgs) {
+    console.log("#pkgs: ", pkgs.length);
   });
+
+  // package has been installed -> begin the mako toolchain
+  instance.on("installed-pkg", function(pkg) {
+    console.log(pkg.name + ": finished -> mako");
+    var inst = new MakoBrowserify({
+      path: outFolder,
+      tmpPath: tmpPath
+    }, pkg);
+    inst.build().then(function() {
+      instance.trigger("done-pkg", pkg);
+    }, function(err) {
+      console.log("Error:", err);
+    });
+  });
+
+  // a single package has been finalised
+  instance.on("done-pkg", function(pkg) {
+    console.log(pkg.name + ": success");
+  });
+
+  // lets start the party
+
+  fsp.remove(outFolder)
+    .then(function() {
+      return fsp.mkdirp(outFolder);
+    }).then(function() {
+      return installer.init();
+    }).then(function() {
+      tmpPath = installer.path;
+      instance.start();
+    }, function(err) {
+      console.log("Error:", err);
+    });
+
+}
